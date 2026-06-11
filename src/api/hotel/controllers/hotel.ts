44593d0413b966ab15934bac3be34f183b2e7bd5
julianbrasse-zhpf <julian.brasse@zhpf.ch>
2026-06-11 10:31:42 +0000

    Install strapi

---
+++ src/api/hotel/controllers/hotel.ts
@@ -0,0 +1,7 @@
+/**
+ * hotel controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::hotel.hotel');
---
+++ src/api/hotel/services/hotel.ts
@@ -0,0 +1,7 @@
+/**
+ * hotel service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::hotel.hotel');
